ffd17e150fc12affb5526a5af78354bdb8e731bf
Marco Ghilardelli <marco.ghilardelli@students.fhnw.ch>
2016-12-19 14:15:42 +0100

    initialize project

---
+++ README.md
@@ -0,0 +1,4 @@
+# grunliberale
+This is a website template for grunliberale.ch
+
+Contributors: Marco Ghilardelli (marco.ghilardelli@students.fhnw.ch), Steven Schmid (steven.schmid@students.fhnw.ch), Luca Filoni (luca.filoni@students.fhnw.ch)
---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    (function ($) {
+        $(function () {
+
+            $('.button-collapse').sideNav();
+            $('.parallax').parallax();
+
+        }); // end of document ready
+    })(jQuery); // end of jQuery name space
+});
