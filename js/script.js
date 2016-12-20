b7b15b81dac42d16e243db1483654f1645d4eb93
Steven Schmid <steven.schmid@students.fhnw.ch>
2016-12-20 13:51:40 +0100

    Dropdown (not finished yet)

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
 $(document).ready(function () {
     (function ($) {
         $(function () {
+            $('.dropdown-button').dropdown({ hover: false });
             $('.slider').slider({full_width: true});
             $('.button-collapse').sideNav();
             $('.parallax').parallax();
+
 
         }); // end of document ready
     })(jQuery); // end of jQuery name space
