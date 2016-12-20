bcd7ce03df1ddbc3f8c9422ab1fd034afe51b15c
Steven Schmid <steven.schmid@students.fhnw.ch>
2016-12-20 16:46:02 +0100

    Dropdown

--- js/script.js
+++ js/script.js
@@ -1,10 +1,17 @@
 $(document).ready(function () {
     (function ($) {
         $(function () {
-            $(".dropdown-button").dropdown();
+            $(".dropdown-button").dropdown({ hover: true });
             $('.slider').slider({full_width: true});
             $('.button-collapse').sideNav();
             $('.parallax').parallax();
+            $(".social").sideNav({
+                    menuWidth: 300, // Default is 240
+                    edge: 'left', // Choose the horizontal origin
+                    closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+                    draggable: true // Choose whether you can drag to open on touch screens
+                }
+            );
 
 
         }); // end of document ready
